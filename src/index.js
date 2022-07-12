6b07bfe069295865d588a921923d6218afc1a41c
Ilya <ilyafomin9283@mail.ru>
2022-07-12 13:10:59 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const array = Array.from(String(n), Number);
+    let newArray = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        if (!isNaN(element)) {
+            newArray.push(element);
+        }
+    }
+
+    newArray.reverse();
+    const string = newArray.join("");
+    return string;
+};
